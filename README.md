87c0bb17b0107878282866948adcf13a0ef0f01e
Daniel Damiani <ddamiani@slac.stanford.edu>
2019-05-10 13:41:16 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# amityping
+Provides typing hints to be shared between LCLS-II analysis packages.
---
+++ setup.py
@@ -0,0 +1,35 @@
+import io
+import re
+from setuptools import setup
+
+with io.open('amityping/__init__.py', 'rt', encoding='utf8') as f:
+    version = re.search(r'__version__ = \'(.*?)\'', f.read()).group(1)
+
+setup(
+    name="amityping",
+    version=version,
+    description='LCLS analysis monitoring type annotations',
+    long_description='The package used at LCLS-II for type annotations for online analysis monitoring',
+    author='Seshu Yamajala, Daniel Damiani',
+    author_email='',
+    url='http://github.com/slac-lcls/amityping',
+    packages=["amityping"],
+    package_data={
+        "amityping": ["py.typed"],
+    },
+    install_requires=['numpy', 'mypy_extensions'],
+    classifiers=[
+        'Development Status :: 1 - Planning'
+        'Environment :: Other Environment',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Utilities',
+    ],
+    zip_safe=False,
+)
