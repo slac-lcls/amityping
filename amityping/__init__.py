e81ea1f3cb83af049883a7269ec533c59f396c65
Daniel Damiani <ddamiani@slac.stanford.edu>
2019-05-10 16:47:59 -0700

    add more possible channels to HSDWaveforms

--- amityping/__init__.py
+++ amityping/__init__.py
@@ -1,5 +1,5 @@
 import numpy as np
-from typing import Union, List
+from typing import Union, List, Tuple, Dict
 from mypy_extensions import TypedDict
 
 
@@ -48,4 +48,27 @@ class Array3d(type):
 Array = Union[Array2d, Array1d, List[float]]
 
 
-HSDWaveforms = TypedDict("HSDWaveforms", {'times': Array1d, '0': Array1d})
+HSDPeaks = Dict[int, Tuple[List[int], List[Array]]]
+
+
+HSDWaveforms = TypedDict("HSDWaveforms",
+                         {
+                          'times': Array1d,
+                          '0': Array1d,
+                          '1': Array1d,
+                          '2': Array1d,
+                          '3': Array1d,
+                          '4': Array1d,
+                          '5': Array1d,
+                          '6': Array1d,
+                          '7': Array1d,
+                          '8': Array1d,
+                          '9': Array1d,
+                          '10': Array1d,
+                          '11': Array1d,
+                          '12': Array1d,
+                          '13': Array1d,
+                          '14': Array1d,
+                          '15': Array1d,
+                         },
+                         total=False)
