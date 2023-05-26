522cebb65da7f5cf96a6475debd0323a1caa84d6
Vincent Esposito <espov@slac.stanford.edu>
2023-05-25 17:29:19 -0700

    Add tuple of floats as valid AMI array types

--- amitypes/array.py
+++ amitypes/array.py
@@ -90,4 +90,4 @@ class Array3d(metaclass=Array3dMeta):
     pass
 
 
-Array = typing.Union[Array3d, Array2d, Array1d, typing.List[float]]
+Array = typing.Union[Array3d, Array2d, Array1d, typing.List[float], typing.Tuple[float, ...]]
